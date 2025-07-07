39e02fdf78338117b039bc9c5c0cfd557b1196ab
Csaba Hegyi <csaba.hegyi@bbc.com>
2025-07-07 15:13:09 +0100

    Revert "Fixing tests"
    This reverts commit 4b1cc4face2532d1b6d9c309b3e1aaf1bf9176b1.

--- internal/engine/executor_test.go
+++ internal/engine/executor_test.go
@@ -243,7 +243,7 @@ func TestMutatorRun(t *testing.T) {
 			pkg:      "example.com/my/package",
 			callDir:  "test/dir",
 			tags:     "tag1,t1g2",
-			wantPath: ".",
+			wantPath: "example.com/my/package",
 		},
 		{
 			name:     "integration mode",
@@ -259,7 +259,7 @@ func TestMutatorRun(t *testing.T) {
 			pkg:                "example.com/my/package",
 			callDir:            "test/dir",
 			tags:               "tag1,t1g2",
-			wantPath:           ".",
+			wantPath:           "example.com/my/package",
 		},
 	}
 	for _, tc := range testCases {
